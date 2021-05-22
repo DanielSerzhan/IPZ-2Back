b4e99b16669f9c07d5cab528983c13b19f5be95c
JenyaVozduhaTochkaNet <72171527+JenyaVozduhaTochkaNet@users.noreply.github.com>
2021-05-22 03:12:11 +0300

    Add files via upload

---
+++ src/main/java/com/myproject/controllers/CORSFilter.java
@@ -0,0 +1,43 @@
+package com.myproject.controllers;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Serzhan Daniil
+ * @version 12.0.2
+ * Cors filter
+ */
+
+@Component
+public class CORSFilter implements Filter {
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @param chain
+     * @throws IOException
+     * @throws ServletException
+     */
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {}
+
+    public void destroy() {}
+
+}
